d8ad21a88a6fb96a9b2079172f489c0734abba2b
Yimin Zhao <tacingiht@gmail.com>
2021-01-22 12:35:29 +0800

    add heapsort impl and entry point

--- sort/sort.hpp
+++ sort/sort.hpp
@@ -1,2 +1,45 @@
 #include "general.hpp"
 
+namespace yao {
+
+class Heap {
+public:
+  Heap(size_t size): sz(0), data(vector<int>(size, 0)) {};
+
+  void push(int x) {
+    int i = sz++;
+    while (i > 0) {
+      int pos = (i - 1) / 2;
+      if (data[pos] <= x) break;
+      data[i] = data[pos];
+      i = pos;
+    }
+    data[i] = x;
+  }
+
+  int pop() {
+    int res = data[0];
+    int x = data[--sz];
+    int i = 0;
+    while (i * 2 + 1 < sz) {
+      int a = i * 2 + 1, b = i * 2 + 2;
+      if (b < sz && data[b] < data[a]) a = b;
+      if (data[a] >= x) break;
+      data[i] = data[a];
+      i = a;
+    }
+    data[i] = x;
+    return res;
+  }
+
+  void sort() {
+    int i = sz;
+    while (i > 0)
+      data[--i] = pop();
+  }
+
+public:
+  vector<int> data;
+  int sz = 0;
+};
+}
